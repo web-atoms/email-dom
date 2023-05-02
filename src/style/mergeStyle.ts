bba4ab88c455c334c16efe521c1031a116378d54
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-05-02 19:48:56 +0530

    Merge fixed

--- src/style/mergeStyle.ts
+++ src/style/mergeStyle.ts
@@ -6,6 +6,9 @@ export function newCSSStyleDeclaration() {
 export default function mergeStyle(... styles: Partial<CSSStyleDeclaration>[]) {
     const s = newCSSStyleDeclaration();
     for (const iterator of styles) {
+        if (!iterator) {
+            continue;
+        }
         for (let index = 0; index < iterator.length; index++) {
             const name = iterator[index];
             let value = iterator[name];
